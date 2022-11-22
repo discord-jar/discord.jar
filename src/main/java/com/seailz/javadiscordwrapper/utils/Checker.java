102f848e499c77af2fee3c32ceb7d10e41a5e07c
Seailz <81972974+IoyoCode@users.noreply.github.com>
2022-11-22 03:27:34 +0000

    feat: update Checker.java code

--- src/main/java/com/seailz/javadiscordwrapper/utils/Checker.java
+++ src/main/java/com/seailz/javadiscordwrapper/utils/Checker.java
@@ -5,6 +5,8 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
+
 /**
  * A utility class for checking arguments and throwing exceptions if they are invalid.
  *
@@ -23,8 +25,7 @@ public class Checker {
     @Contract("null, _ -> fail")
     public static void isSnowflake(@Nullable String id, @NotNull String message) {
         notNull(id, message);
-        if (!id.matches("[0-9]{17,19}"))
-            throw new IllegalArgumentException(message);
+        if (!id.matches("[0-9]{17,19}")) illegalArgument(message);
     }
 
     /**
@@ -35,7 +36,60 @@ public class Checker {
      */
     @Contract("null, _ -> fail")
     public void notNull(@Nullable Object object, @NotNull String message) {
-        if (object == null) throw new IllegalArgumentException(message);
+        if (object == null) nullArgument(message);
+    }
+
+    /**
+     * Checks if a given string matches a given regex.
+     *
+     * @param regex The regex to match.
+     * @param string The string to check.
+     * @param message The message to throw if the string does not match the regex.
+     */
+    public void matches(String regex, String string, String message, boolean invert) {
+        if (string.matches(regex)) illegalArgument(message);
+        if (invert)
+            if (!string.matches(regex)) illegalArgument(message);
+    }
+
+    public void matches(String regex, String string, String message) {
+        matches(regex, string, message, false);
+    }
+
+    /**
+     * Checks the length of a given string.
+     *
+     * @param length The length to check.
+     * @param string The string to check.
+     * @param message The message to throw if the string is not the given length.
+     */
+    public void length(int length, String string, String message) {
+        matches("{" + length + "}", string, message);
+    }
+
+    public void size(List<?> list, int size, String message) {
+        if (list.size() != size) illegalArgument(message);
+    }
+
+    public void sizeLessThan(List<?> list, int size, String message) {
+        if (list.size() > size) illegalArgument(message);
+    }
+
+    public void sizeGreaterThan(List<?> list, int size, String message) {
+        if (list.size() < size) illegalArgument(message);
+    }
+
+    private void illegalArgument(String message) {
+        throw new IllegalArgumentException(message);
+    }
+    private void nullArgument(String message) {
+        throw new NullArgumentException(message);
+    }
+
+    public class NullArgumentException extends IllegalArgumentException {
+        public NullArgumentException(String message) {
+            super(message);
+        }
     }
 
 }

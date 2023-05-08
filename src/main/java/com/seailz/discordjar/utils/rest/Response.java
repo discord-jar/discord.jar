061a7367af5ecc7916ba4808fb7e0391e6f2d2e3
IoyoCode <me@ioyo.dev>
2023-05-08 20:06:56 +0100

    feat(error handling): added blocking methods

--- src/main/java/com/seailz/discordjar/utils/rest/Response.java
+++ src/main/java/com/seailz/discordjar/utils/rest/Response.java
@@ -5,6 +5,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -18,22 +19,66 @@ public class Response<T> {
 
     public T response;
     public Error error;
+    public boolean completed = false;
 
     public List<Consumer<T>> onCompletion = new ArrayList<>();
     public List<Consumer<Error>> onError = new ArrayList<>();
 
     public Response() {}
 
+    /**
+     * Blocks the current thread until the response is received, or an error occurs.
+     * @return Either the response or null if an error occurred.
+     */
+    public T awaitCompleted() {
+        while (!completed) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return response; // Response will be null if an error occurred
+    }
+
+    /**
+     * Blocks the current thread until an error occurs, or the response is received.
+     * @return Either the error or null if the response was received.
+     */
+    public Error awaitError() {
+        while (!completed) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return error; // Error will be null if no error occurred
+    }
+
+    public Error getError() {
+        return error;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
     public Response<T> complete(T response) {
         this.response = response;
         for (Consumer<T> tConsumer : onCompletion) {
             tConsumer.accept(response);
         }
+        completed = true;
         return this;
     }
 
     public Response<T> completeError(Error error) {
         this.error = error;
+        for (Consumer<Error> errorConsumer : onError) {
+            errorConsumer.accept(error);
+        }
+        completed = true;
         return this;
     }
 

be6f8eff938843f636224d98a2f72d58ebe2bb19
itstotallyjan <jan.santi@icloud.com>
2023-05-15 11:22:03 -0400

    javadoc: add javadocs for GuildIntegration

--- src/main/java/com/seailz/discordjar/model/guild/GuildIntegration.java
+++ src/main/java/com/seailz/discordjar/model/guild/GuildIntegration.java
@@ -1,5 +1,17 @@
 package com.seailz.discordjar.model.guild;
 
+/**
+ * Represents a Guild Integration.
+ * @param id The id of the integration.
+ * @param name The name of the integration.
+ * @param type The type of the integration.
+ * @param enabled The enabled state of the integration.
+ * @param syncing The syncing state of the integration.
+ * @param roleId The id of the role associated with the integration.
+ * @param enableEmoticons Whether emoticons are enabled.
+ * @param expireBehavior The expiry behavior of the integration.
+ * @param expireGracePeriod The grace period time of the integration's expiration.
+ */
 public record GuildIntegration(
         long id,
         String name,

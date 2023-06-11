81599e4b91631363e7f7149d4fd49fa9c3d5e685
IoyoCode <me@ioyo.dev>
2023-06-11 09:53:29 +0100

    feat(gateway): if the gateway connection is not open, it will be restarted.

--- src/main/java/com/seailz/discordjar/gateway/GatewayFactory.java
+++ src/main/java/com/seailz/discordjar/gateway/GatewayFactory.java
@@ -90,6 +90,20 @@ public class GatewayFactory extends TextWebSocketHandler {
             this.gatewayUrl = URLS.GATEWAY.BASE_URL;
         } else this.gatewayUrl = response.body().getString("url");
         connect();
+
+        // Set up a new thread to monitor the gateway connection and force a reconnect if it's not active.
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (!session.isOpen()) {
+                    discordJar.restartGateway();
+                }
+            }
+        }).start();
     }
 
     public void connect(String customUrl) throws ExecutionException, InterruptedException {
